51d207626cd9a1b3829d2f5e5338a9b7ca6f7d6c
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-04-08 23:55:17 +0200

    Implementacja endpoint'ów dla kryptografii symetrycznej

--- Ćwiczenie 4/WebAPI_crypto/src/main.py
+++ Ćwiczenie 4/WebAPI_crypto/src/main.py
@@ -1,5 +1,36 @@
 # -*- coding: utf-8 -*-
 
+from crypto.symetric import Symetric
+from crypto.asymetric import Asymetric
+
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
+
+@app.get("/symetric/key")
+def read_symmetric_key():
+    return {"Generated symetric key: ": Symetric.generateKey()}
+
+@app.post("/symetric/key")
+def create_symmetric_key(key: str):
+    return key
+
+@app.post("/symetric/encode")
+def create_token(message: bytes):
+    
+    key = Symetric.generateKey()
+    token = Symetric.encode(message, key)
+    
+    return token
+
+@app.post("/symetric/decode")
+def create_message(token: bytes):
+    
+    key = Symetric.generateKey()
+    message = Symetric.decode(token, key)
+    
+    return message
+
+# TODO endpointy dla asymetrycznej
