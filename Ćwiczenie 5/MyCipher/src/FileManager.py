da423f56c4c4f0f3fdfbf498851f40de2fff1f36
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-04-15 15:41:34 +0200

    Wstępna struktura plików/programu

---
+++ Ćwiczenie 5/MyCipher/src/FileManager.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+class FileManager:
+    
+    """ Klasa statyczna, odpowiedzialna za operacje na plikach (odczyt, zapis) """
+    
+    @staticmethod
+    def readFile(filePath: str) -> [str]:
+        
+        """ Odczytuje zawartosc pliku tekstowego, zwracając listę wierszy """
+        
+        pass
+        
+    @staticmethod
+    def saveFile(file: str, filePath: str):
+        
+        """ Zapisuje dane do pliku tekstowego """
+        
+        pass
