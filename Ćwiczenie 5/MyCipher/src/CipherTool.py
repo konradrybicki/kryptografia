80b3718e645c2875fad271c1a7fea0f247af4ff0
Konrad Rybicki <konradrybicki.altamoda@gmail.com>
2021-04-15 23:45:54 +0200

    Klasa CipherTool - implementacja metody generującej klucz

--- Ćwiczenie 5/MyCipher/src/CipherTool.py
+++ Ćwiczenie 5/MyCipher/src/CipherTool.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 class CipherTool:
     
     """ Klasa, odpowiadająca za operacje kryptograficzne """
@@ -14,7 +16,40 @@ class CipherTool:
         
         """ Generuje klucz, inicjalizując pole 'key' """
         
-        pass
+        cryptogramAlphabet = [
+            
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+            "u", "v", "w", "x", "y", "z",
+                              
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+            "K", "K", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z",
+                              
+            "0", "1"
+            
+        ]
+        
+        cipherKey = {}
+        
+        for i in range(27):
+            
+            cipherKey_letter = ""
+            cipherKey_substitutions = []
+            
+            if i < 26:
+                cipherKey_letter = chr(97+i) # małe litery alfabetu angielskiego (ASCII)
+            else:
+                cipherKey_letter = " "
+            
+            for j in range(2):
+                substitution = random.choice(cryptogramAlphabet)
+                cipherKey_substitutions.append(substitution)
+                cryptogramAlphabet.remove(substitution)
+                
+            cipherKey.update({cipherKey_letter: cipherKey_substitutions})
+            
+        self.key = cipherKey
     
     def encrypt(self, plainText: [str]) -> [str]:
         
